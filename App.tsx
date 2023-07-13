bc562f766379448390ee029f2ac36a37a6e159bc
ravindersharma83 <ravinder200198@gmail.com>
2023-07-13 17:18:05 +0530

    Implementing Stack and Drawer Navigation , move google and navigation code on seperate files

--- App.tsx
+++ App.tsx
@@ -1,38 +1,12 @@
-import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import { _signInWithGoogle } from './src/config/firebase/GoogleSignIn'
-import { _signOut } from './src/config/firebase/GoogleSignOut';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import DrawerNavigation from './src/Navigation/DrawerNavigation'
+import GoogleLogin from './src/Screens/GoogleLogin'
 
 const App = () => {
-  const[userInfo,setUserInfo]:any = useState(null);
-  const googleSignin = async()=>{
-    _signInWithGoogle().then(data => {
-      if(!data) {
-        console.log('=>Error','No Data');
-        return;
-      }
-      console.log('=>Success',data);
-      setUserInfo(data);
-      
-    })
-  }
-
-  const signOut = async () => {
-    _signOut().then(data => {
-      if(data) {
-        setUserInfo(null);
-        Alert.alert("Signing out ...")
-      }
-    })
-  };
-
   return (
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-       {userInfo != null && (<Image source={{uri:userInfo.user.photo}} style={{width:100,height:100,borderRadius:50}}/>)}
-       {userInfo != null && <Text>{userInfo.user.name}</Text>}
-       {userInfo != null && <Text>{userInfo.user.email}</Text>}
-       {userInfo ==null ? (<Button onPress={()=>googleSignin()} title='Google Login'/>) : (<Button onPress={()=>signOut()} title='Sign Out'/>)}
-      
+    <View>
+      <Text>Hello Ravinder</Text>
     </View>
   )
 }
@@ -40,3 +14,4 @@ const App = () => {
 export default App
 
 const styles = StyleSheet.create({})
+
